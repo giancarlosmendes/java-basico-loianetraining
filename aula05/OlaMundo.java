b503df7e4693ae4f834cb039cb5cbceedc50b033
Giancarlos Mendes <gmendescamara@gmail.com>
2025-09-05 23:40:54 -0300

    Adicionadas aula05, aula06 e aula07

---
+++ aula05/OlaMundo.java
@@ -0,0 +1,14 @@
+/*
+    Lição de Casa Aula 05:
+    1. Criar um arquivo chamado OlaMundo.java
+    2. Implementar um programa Java que escreva na tela "Olá, Mundo!"
+    3. Compilar e Executar o programa
+*/
+
+class OlaMundo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá, Mundo!");
+    }
+}
